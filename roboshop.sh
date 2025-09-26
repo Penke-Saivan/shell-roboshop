67cd6d8becef63161a14c097d9ab7adaadab5754
SaiPavan-Penke <pavantonystark99@gmail.com>
2025-09-26 19:12:31 +0530

    if -syntax

--- roboshop.sh
+++ roboshop.sh
@@ -10,5 +10,6 @@ do
         IP_Address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     else
         IP_Address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text) 
+    fi    
 
 done
