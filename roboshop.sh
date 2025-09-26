1bcc69434ce1c993687c9226dc38a9abbcd544a8
SaiPavan-Penke <pavantonystark99@gmail.com>
2025-09-26 19:08:33 +0530

    messages

--- roboshop.sh
+++ roboshop.sh
@@ -1,6 +1,6 @@
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0adfe78ec6189cd05"
-
+echo "get IP_s"
 for instance in $*
 do 
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
