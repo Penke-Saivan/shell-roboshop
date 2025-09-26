30e32598c6a75225ba1842916ade26fa74863f05
SaiPavan-Penke <pavantonystark99@gmail.com>
2025-09-26 19:04:42 +0530

    GetIPs

---
+++ roboshop.sh
@@ -0,0 +1,13 @@
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0adfe78ec6189cd05"
+
+for instance in $*
+do 
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --count 1 --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+
+    if [ $instance != "frontend" ]; then 
+        IP_Address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+    else
+        IP_Address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text) 
+
+done
